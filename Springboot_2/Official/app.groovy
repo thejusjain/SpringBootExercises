6208c59a3324c0df8d0aed212f6185218b619fb0
thejusjain <thejusjain101@gmail.com>
2020-05-12 12:09:32 +0530

    Cleaned projects

--- Springboot_2/Official/app.groovy
+++
@@ -1,9 +0,0 @@
-@RestController
-
-class ThisWillActuallyRun {
-
-@RequestMapping("/")
-String home() {
-	"hello World!"
-}
-}
--- taraperformanceweb/src/main/java/com/example/taraperformanceweb/service/SecurityService.java
+++
@@ -1,7 +0,0 @@
-package com.example.taraperformanceweb.service;
-
-public interface SecurityService {
-//    String findLoggedInUsername();
-
-    void login(String username, String password);
-}
